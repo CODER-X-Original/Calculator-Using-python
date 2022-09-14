a29e7a4ef3b98a78ad18b6b1734fd41fc2a610e7
Coder X <110594137+CODER-X-Original@users.noreply.github.com>
2022-09-14 20:18:15 +0500

    Initial commit

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,4 @@
+
 print("Welcome to the calculator")
 print("select an operation to perform:")
 print("1. ADD")
@@ -5,24 +6,17 @@ print("2. SUBTRACT")
 print("3. DIVIDE")
 print("4. MULTIPLY")
 
-operation =input()
+operation = input("What is your operation : ")
+num1 = float(input("Enter Number 1 : "))
+num2 = float(input("Enter Number 2 : "))
 
 if operation == "1":
-    num1 = input("Enter Number 1:")
-    num2 = input("Enter Number 2:")
-    print (int(num1) + int(num2))
+    print ("Num 1 + Num 2 = " , float(num1) + float(num2)) or print("Num 2 + Num 1 = " , float(num1) + float(num2))
 elif operation == "2":
-    num1 = input("Enter Number 1:")
-    num2 = input("Enter Number 2:")
-    print (int(num1) - int(num2))
+    print ("Num 1 - Num 2 = " , float(num1) - float(num2)) or print("Num 2 - Num 1 = " , float(num1) - float(num2))
 elif operation == "3":
-    num1 = input("Enter Number 1:")
-    num2 = input("Enter Number 2:")
-    print (int(num1) / int(num2))
+    print ("Num 1 / Num 2 = " , float(num1) / float(num2)) or print("Num 2 / Num 1 = " , float(num1) / float(num2))
 elif operation == "4":
-    num1 = input("Enter Number 1:")
-    num2 = input("Enter Number 2:")
-    print (int(num1) * int(num2))
+    print ("Num 1 * Num 2 = " , float(num1) * float(num2)) or print("Num 2 * Num 1 = " , float(num1) * float(num2))
 else:
     print("Invalid Entry")
-
