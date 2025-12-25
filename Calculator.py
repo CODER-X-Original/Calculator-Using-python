86e8a9a1a7426f4c61336074935f550373fc801f
CODERXOFFICIAL <110594137+CODER-X-Original@users.noreply.github.com>
2025-12-25 15:22:55 +0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -5,6 +5,7 @@ print("1. ADD")
 print("2. SUBTRACT")
 print("3. DIVIDE")
 print("4. MULTIPLY")
+print("\nKindly choose the operation above and enter it's    corresponding number")
 
 operation = input("What is your operation : ")
 num1 = float(input("Enter Number 1 : "))
@@ -19,4 +20,5 @@ elif operation == "3":
 elif operation == "4":
     print ("Num 1 * Num 2 = " , float(num1) * float(num2)) or print("Num 2 * Num 1 = " , float(num1) * float(num2))
 else:
+
     print("Invalid Entry")
